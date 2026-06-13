758c376094359aa3c6699c7b459183be44cdc5bc
narencludev <narencloudev@gmail.com>
2026-06-14 00:43:40 +0530

    initial dotfiles

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font("MesloLGS Nerd Font"),
+  font_size = 10.0,
+
+  color_scheme = "Gruvbox Dark (Gogh)",
+
+  initial_cols = 120,
+  initial_rows = 40,
+}
